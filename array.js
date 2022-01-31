50b9aa0a35cf8adfc5b28bf558e745e5327b95f3
ShubhamD1x1T <dixitshubham189@gmail.com>
2022-01-31 23:08:46 +0530

    array operationa

--- array.js
+++ array.js
@@ -1,8 +1,8 @@
 //array declaration
 
-let arr = [1,2,3,4,5];
+let arr1 = [1,2,3,4,5];
 
-console.log(arr);
+console.log(arr1);
 
 let b = [1,'c',"i am string",true];
 
@@ -21,3 +21,33 @@ console.log(b[4]);
 
 b[9]="I am coming";
 console.log(b);
+
+// Array Operations
+
+//1.push
+
+let arr = [1,2,3,4,5];
+
+console.log("befor push",arr);
+
+arr.push(6);
+
+console.log("after push",arr);
+
+//2.pop
+
+console.log("before pop",arr);
+arr.pop();
+console.log("after pop",arr);
+
+//3.shift
+
+console.log("before shift",arr);
+arr.shift();
+console.log("after shift",arr);
+
+//4.Unshift
+
+console.log("before unshift",arr);
+arr.unshift(6);
+console.log("after unshift",arr);
