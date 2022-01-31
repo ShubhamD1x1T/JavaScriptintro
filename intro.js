8484d09e4278d82bf8d4bc2ede98faa3645c88da
ShubhamD1x1T <dixitshubham189@gmail.com>
2022-01-31 21:27:07 +0530

    Js intro deploy

---
+++ intro.js
@@ -0,0 +1,18 @@
+//print hello world
+
+
+console.log("Hello World");
+
+let name = "shubham";
+
+console.log(name);
+
+let num = 1;
+
+let  ch = 'a';
+
+let bool = true;
+
+console.log(num);
+console.log(ch);
+console.log(bool);
